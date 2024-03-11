d88c856f7f5726f8c5cd8f92b0c4e2c787ca91aa
Chelsea - FIC <c.hansen.mph@gmail.com>
2024-03-11 16:34:28 -0400

    Updating with new model structure (MSIRS)

---
+++ 1. Data/data_prep.R
@@ -0,0 +1,151 @@
+rm(list=ls())
+library(deSolve)
+library(RColorBrewer)
+library(reshape2)
+library(tidyverse)
+library(ggplot2)
+library(cowplot)
+library(sjstats)
+library(cdcfluview)
+library(zoo)
+library(imputeTS)
+library(readxl)
+library(tidycensus)
+
+"%notin%" = Negate('%in%')
+setwd("C:/Users/hansencl/OneDrive - National Institutes of Health/Desktop/GitHub Updates/Data")
+
+#This code prepares the demographic data needed to calibrate the model, including: 
+  #1. Population size 
+  #2. birth rates
+  #3. net mortality and migration rates to recreate population growth 
+
+
+
+# 1. Population size ------------------------------------------------------
+
+#Get population estimates for the most recent population size estimate 
+#This will be scaled down to the 1995 population size for the burn-in period 
+#you can fill in the initiate_compartments spreadsheet or use the code below 
+pop = get_estimates(
+  geography = "county",
+  product = "characteristics",
+  breakdown = c("AGEGROUP"),
+  breakdown_labels = TRUE,
+  state = "WA",
+  county = "King",
+  year = 2022)%>% 
+  select(AGEGROUP,value) 
+new_agegrps = c("all","0to4","5to9",rep("10to19",2),rep("20-39",4),rep("40to59",4),rep("60+",6))
+
+pop = pop %>% 
+  mutate(new_age = factor(new_agegrps,levels=unique(new_agegrps))) %>% 
+  group_by(new_age) %>% 
+  summarize(population = sum(value)) 
+
+#total population in 2022 (most recent data available )
+pop_2022 = pop %>% filter(new_age=="all") 
+pop_2022=pop_2022$population
+
+#total population in 1995 (start of burn-in period)
+pop_1995 = readRDS("Demographic Data/birth_rates_by_county.rds") %>% 
+  filter(state=="WA",county=="King County",year==1995)
+pop_1995=pop_1995$population
+
+
+
+#This will look the same as the initiate_compartments spreadsheet 
+M = c(rep(pop[2,2]*.034,3),rep(0,10))
+S0 = c(rep(0,3),rep(pop[2,2]*.034,3),pop[2,2]*.2,pop[2,2]*.6,pop[3,2],pop[4,2],pop[5,2],pop[6,2],pop[7,2])
+I1 = c(rep(0,3),rep(1,10))
+R1 = rep(0,13)
+S1 = rep(0,13)
+I2 = rep(0,13)
+R2 = rep(0,13)
+S2 = rep(0,13)
+I3 = rep(0,13)
+R3 = rep(0,13)
+S3 = rep(0,13)
+I4 = rep(0,13)
+R4 = rep(0,13)
+Mn = rep(0,13)
+Mv = rep(0,13)
+N = rep(0,13)
+Si = rep(0,13)
+Vs1 = rep(0,13)
+Vs2 = rep(0,13)
+yinit=matrix(unlist(cbind(M,S0,I1,R1,S1,I2,R2,S2,I3,R3,S3,I4,R4,Mn,Mv,N,Si,Vs1,Vs2)),byrow=FALSE,ncol=19)
+yinit[,1:2]= yinit[,1:2]*(pop_1995/pop_2022)#adjust population size to 1995 
+colnames(yinit) = c("M","S0","I1","R1","S1","I2","R2","S2","I3","R3","S3","I4","R4","Mn","Mv","N","Si","Vs1","Vs2")
+saveRDS(yinit,"Demographic Data/yinit_interventions.rds") #save the full version 
+saveRDS(yinit[,1:13],"Demographic Data/yinit.rds")#save a version that does not have the intervention compartments 
+
+#format the data for feeding into the model 
+N_ages <- nrow(yinit) 
+agenames <- c("<2m","2-3m","4-5m","6-7m","8-9m","10-11m","1Y","2-4Y","5-9Y","10-19Y","20-39Y","40-64Y","65Y+")
+al <- N_ages
+rownames(yinit) <- agenames
+yinit.vector <- as.vector(unlist(yinit))
+name.array <- array(NA, dim=dim(yinit))
+for(i in 1:dim(name.array)[1]){
+  for(j in 1:dim(name.array)[2]){
+    name.array[i,j] <- paste(dimnames(yinit)[[1]][i],dimnames(yinit)[[2]][j]  )
+  }
+}
+name.vector <- as.vector(name.array)
+names(yinit.vector) <- name.vector
+
+saveRDS(yinit.vector, "Demographic Data/yinit.vector_interventions.rds")#save the full version 
+saveRDS(yinit.vector[1:169], "Demographic Data/yinit.vector.rds")#save a version without the intervention compartments 
+
+#seeding 1 infection per 100,000 (based on 2022 population)
+seed = pop_2022/100000 #seeding 1 infection per 100,000 (based on 2022 population)
+
+
+# 2. Birth Rates --------------------------------------------------------
+births = readRDS("Demographic Data/birth_rates_by_county.rds") %>% 
+  filter(state=="WA",county=="King County") %>% 
+  mutate(mmwr_year=as.numeric(year),
+         mmwr_week=as.numeric(1)) %>% 
+  select(mmwr_year,mmwr_week, birth_rate) 
+
+fill_in = data.frame(date =seq(from=as.Date('1995-01-07'), to=as.Date("2025-10-01"), by=7)) %>% 
+  mutate(mmwr_week(date))
+
+birth_interp = births %>% 
+  right_join(fill_in, by=c("mmwr_year","mmwr_week")) %>% 
+  arrange(date) %>% 
+  mutate(birth_rate = na_locf(birth_rate))
+#for the 2023-2025 data we are just assuming the same as 2022 (most recent available data)
+
+#matrix with column for each age group - for all age groups except the youngest birth rate = 0
+birth_complete = data.frame(date = birth_interp$date,
+                   V1 = birth_interp$birth_rate, 
+                   V2 = rep(0,nrow(birth_interp)),
+                   V3 = rep(0,nrow(birth_interp)),
+                   V4 = rep(0,nrow(birth_interp)),
+                   V5 = rep(0,nrow(birth_interp)),
+                   V6 = rep(0,nrow(birth_interp)),
+                   V7 = rep(0,nrow(birth_interp)),
+                   V8 = rep(0,nrow(birth_interp)),
+                   V9 = rep(0,nrow(birth_interp)),
+                   V10 = rep(0,nrow(birth_interp)),
+                   V11 = rep(0,nrow(birth_interp)),
+                   V12 = rep(0,nrow(birth_interp)),
+                   V13 = rep(0,nrow(birth_interp)))
+
+
+saveRDS(birth_complete,"Demographic Data/births_kingcounty.rds")
+
+
+
+# 3. Net death and migration ----------------------------------------------
+#estimated value to use to recreate the population growth
+#will check later in the model, this value may need to be adjusted slightly 
+
+weeks = 1461 #number of weeks from January 1995 to December 2022 (check in births data frame)
+um = ((pop_2022-pop_1995)/weeks)/((pop_2022+pop_1995)/2)
+
+#save the parameters
+other_parms = c("seed"=seed, "um"=um,"pop"=pop_2022)
+saveRDS(other_parms,"Demographic Data/other_parms.rds")
---
+++ 3. Interventions/MSIRS_scenario_functions.R
@@ -0,0 +1,272 @@
+interventions = function(birth_dose, #timing and coverage of nirsevimab birth doses
+                         cover_n, #timing and coverage of nirsevimab catch-up doses
+                         waningN, #duration of nirsevimab protection (in days)
+                         RRIn, #relative risk of infection while protected by nirsevimab (default = 1)
+                         RRHn,#relative risk of hospitalization while protected by nirsevimab
+                         maternal_dose, #timing and coverage of infants born following maternal doses
+                         waningV, #duration of maternal vaccination protection (in days)
+                         RRIv, #relative risk of infection while protected by maternal vaccination (default = 1)
+                         RRHv,#relative risk of hospitalization while protected by maternal vaccination 
+                         cover_s, #timing and coverage of senior vaccination 
+                         waningS, #duration of protection from vaccines (in days) for seniors
+                         RRIs, #relative risk of infection after vaccination in seniors (default = 1)
+                         RRHs #relative risk of hospitalization after vaccination in seniors
+){
+  
+  
+  parmset<-list(PerCapitaBirthsYear=birth,
+                WidthAgeClassMonth=c(rep(2,times=6), 12,12*3, 60, 120, 240, 240, 240),
+                DurationMatImmunityDays=112,
+                baseline.txn.rate=baseline.txn.rate,
+                RRHm = 0.7,
+                b1=b1,
+                phi=phi,
+                recover1 = 365.25*.5,
+                recover2 = 365.25*.5,
+                recover3 = 365.25,
+                recover4 = 365.25, 
+                age_reporting=age_reporting,
+                um=um,
+                rho1=0.75,
+                rho2=0.51,
+                dur.days1=10,
+                dur.days2=7,
+                dur.days3=5,
+                yinit.matrix=yinit,
+                q=1,
+                c2=contact,
+                sigma1=0.76,
+                sigma2=0.6,
+                sigma3=0.4,
+                npi=npi,
+                introductions = introductions,
+                time.step='week',
+                #intervention parameters
+                birth_dose = birth_dose, 
+                cover_n = cover_n,
+                waningN = waningN,
+                RRIn = RRIn,
+                RRHn = RRHn,
+                maternal_dose = maternal_dose, 
+                waningV = waningV,
+                RRIv = RRIv,
+                RRHv = RRHv,
+                cover_s = cover_s, 
+                waningS = waningS, 
+                RRIs = RRIs, 
+                RRHs =RRHs
+  )
+  
+  
+  output <- ode(y=yinit.vector, t=times,
+                method = "ode45",
+                func=MSIRS_interventions, 
+                parms=c(parmset))
+  
+  
+  t0 <- tmax3-tmax0+1 #just save results after burn-in period
+  al <- nrow(yinit)
+  output <- tail(output,t0)
+  St <- output[,-1]
+  
+  I1 <- St[,grep('I1', colnames(St))]
+  I2 <- St[,grep('I2', colnames(St))]
+  I3 <- St[,grep('I3', colnames(St))]
+  I4 <- St[,grep('I4', colnames(St))]
+  R1 <- St[,grep('R1', colnames(St))]
+  R2 <- St[,grep('R2', colnames(St))]
+  R3 <- St[,grep('R3', colnames(St))]
+  R4 <- St[,grep('R4', colnames(St))]
+  S0 <- St[,grep('S0', colnames(St))]
+  Si <- St[,grep('Si', colnames(St))]
+  S1 <- St[,grep('S1', colnames(St))]
+  S2 <- St[,grep('S2', colnames(St))]
+  S3 <- St[,grep('S3', colnames(St))]
+  
+  M <- St[,grep('M', colnames(St))][,1:13]
+  Mn <- St[,grep('Mn', colnames(St))]
+  Mv <- St[,grep('Mv', colnames(St))]
+  N <- St[,grep('N', colnames(St))]
+
+  
+  Vs1 <- St[,grep('Vs1', colnames(St))]
+  Vs2 <- St[,grep('Vs2', colnames(St))]
+  
+  contact2 = npi[tmax0:tmax3]
+  intro2 = introductions[tmax0:tmax3]
+  
+  lambda1=matrix(0,nrow=t0,ncol=al)#Force of infection
+  for (t in 1:t0){
+    beta <-  baseline.txn.rate/(parmset$dur.days1/7)/(sum(parmset$yinit.matrix)^(1-parmset$q))*parmset$c2*contact2[t]
+    lambda1[t,] <- as.vector((1+b1*cos(2*pi*(t-phi*52.1775)/52.1775))*((I1[t,]+parmset$rho1*I2[t,]+parmset$rho2*I3[t,]+parmset$rho2*I4[t,]+intro2[t])%*%beta)/sum(St[t,]))}
+  
+ hosp = c(rep(parmset$age_reporting[1],3),rep(parmset$age_reporting[2],3),rep(parmset$age_reporting[3],2),rep(parmset$age_reporting[4],4),parmset$age_reporting[5])
+  
+  H1=matrix(0,nrow=t0,ncol=al)#Number of hospitalizations by age
+  for (i in 1:al){
+    H1[,i]=
+      hosp[i]*parmset$RRHm*parmset$sigma3*M[,i]*lambda1[,i]+
+      hosp[i]*parmset$RRHn*parmset$RRIn*parmset$sigma3*Mn[,i]*lambda1[,i]+
+      hosp[i]*parmset$RRHv*parmset$RRIv*parmset$sigma3*Mv[,i]*lambda1[,i]+
+      hosp[i]*parmset$RRHn*parmset$RRIn*N[,i]*lambda1[,i]+
+      hosp[i]*S0[,i]*lambda1[,i]+
+      hosp[i]*Si[,i]*lambda1[,i]+
+      hosp[i]*parmset$sigma1*S1[,i]*lambda1[,i]+
+      hosp[i]*parmset$sigma2*S2[,i]*lambda1[,i]+
+      hosp[i]*parmset$sigma3*S3[,i]*lambda1[,i]+
+      hosp[i]*parmset$RRIs*parmset$RRHs*parmset$sigma3*Vs1[,i]*lambda1[,i]+
+      hosp[i]*parmset$RRIs*parmset$RRHs*parmset$sigma3*Vs2[,i]*lambda1[,i]}
+  
+  
+  H2= cbind(rowSums(H1[,1:3]),rowSums(H1[,4:6]),rowSums(H1[,7:8]),rowSums(H1[,9:12]),H1[,13]) 
+  results = data.frame(cbind(H2[(t0-(tmax3-tmax2)):t0,],rowSums(H2[(t0-(tmax3-tmax2)):t0,])))
+  names(results)=c("<6m",">6m","1-4yr","5-59yrs","60+yrs","All ages")
+  results$date = dates$date[tmax2:tmax3]
+  
+  
+  return(results)
+}
+
+
+
+
+
+
+interventions_PI = function(birth_dose, #timing and coverage of nirsevimab birth doses
+                             cover_n, #timing and coverage of nirsevimab catch-up doses
+                             waningN, #duration of nirsevimab protection (in days)
+                             RRIn, #relative risk of infection while protected by nirsevimab (default = 1)
+                             RRHn,#relative risk of hospitalization while protected by nirsevimab
+                             maternal_dose, #timing and coverage of infants born following maternal doses
+                             waningV, #duration of maternal vaccination protection (in days)
+                             RRIv, #relative risk of infection while protected by maternal vaccination (default = 1)
+                             RRHv,#relative risk of hospitalization while protected by maternal vaccination 
+                             cover_s, #timing and coverage of senior vaccination 
+                             waningS, #duration of protection from vaccines (in days) for seniors
+                             RRIs, #relative risk of infection after vaccination in seniors (default = 1)
+                             RRHs #relative risk of hospitalization after vaccination in seniors
+){
+  results = data.frame(`<6m`=NA,`>6m`=NA,`1-4yr`=NA,`5-59yrs`=NA,`60+yrs`=NA,`All ages`=NA,date=NA,sample=NA)
+  names(results)=c("<6m",">6m","1-4yr","5-59yrs","60+yrs","All ages","date","sample") 
+  
+  for(l in 1:rep_num){
+    parmset<-list(PerCapitaBirthsYear=birth,
+                  WidthAgeClassMonth=c(rep(2,times=6), 12,12*3, 60, 120, 240, 240, 240),
+                  DurationMatImmunityDays=112,
+                  baseline.txn.rate=baseline.txn.rate,
+                  RRHm = 0.7,
+                  b1=new_parms[l,"b1"],
+                  phi=new_parms[l,"phi"],
+                  recover1 = 365.25*.5,
+                  recover2 = 365.25*.5,
+                  recover3 = 365.25,
+                  recover4 = 365.25, 
+                  h1=new_parms[l,"h1"],
+                  h2=new_parms[l,"h2"],
+                  h3=new_parms[l,"h3"],
+                  h4=new_parms[l,"h4"],
+                  h5=new_parms[l,"h5"],
+                  um=um,
+                  rho1=0.75,
+                  rho2=0.51,
+                  dur.days1=10,
+                  dur.days2=7,
+                  dur.days3=5,
+                  yinit.matrix=yinit,
+                  q=1,
+                  c2=contact,
+                  sigma1=0.76,
+                  sigma2=0.6,
+                  sigma3=0.4,
+                  npi=npi,
+                  introductions = introductions,
+                  time.step='week',
+                  #intervention parameters
+                  birth_dose = birth_dose, 
+                  cover_n = cover_n,
+                  waningN = waningN,
+                  RRIn = RRIn,
+                  RRHn = RRHn,
+                  maternal_dose = maternal_dose, 
+                  waningV = waningV,
+                  RRIv = RRIv,
+                  RRHv = RRHv,
+                  cover_s = cover_s, 
+                  waningS = waningS, 
+                  RRIs = RRIs, 
+                  RRHs =RRHs
+                  )
+
+    
+    output <- ode(y=yinit.vector, t=times,
+                  method = "ode45",
+                  func=MSIRS_interventions, 
+                  parms=c(parmset))
+    
+    
+    t0 <- tmax3-tmax0+1 #just save results after burn-in period
+    al <- nrow(yinit)
+    output <- tail(output,t0)
+    St <- output[,-1]
+    
+    I1 <- St[,grep('I1', colnames(St))]
+    I2 <- St[,grep('I2', colnames(St))]
+    I3 <- St[,grep('I3', colnames(St))]
+    I4 <- St[,grep('I4', colnames(St))]
+    R1 <- St[,grep('R1', colnames(St))]
+    R2 <- St[,grep('R2', colnames(St))]
+    R3 <- St[,grep('R3', colnames(St))]
+    R4 <- St[,grep('R4', colnames(St))]
+    S0 <- St[,grep('S0', colnames(St))]
+    Si <- St[,grep('Si', colnames(St))]
+    S1 <- St[,grep('S1', colnames(St))]
+    S2 <- St[,grep('S2', colnames(St))]
+    S3 <- St[,grep('S3', colnames(St))]
+    
+    M <- St[,grep('M', colnames(St))][,1:13]
+    Mn <- St[,grep('Mn', colnames(St))]
+    Mv <- St[,grep('Mv', colnames(St))]
+    N <- St[,grep('N', colnames(St))]
+    
+    Vs1 <- St[,grep('Vs1', colnames(St))]
+    Vs2 <- St[,grep('Vs2', colnames(St))]
+    
+    contact2 = npi[tmax0:tmax3]
+    intro2 = introductions[tmax0:tmax3]
+    
+    lambda1=matrix(0,nrow=t0,ncol=al)#Force of infection
+    for (t in 1:t0){
+      beta <-  baseline.txn.rate/(parmset$dur.days1/7)/(sum(parmset$yinit.matrix)^(1-parmset$q))*parmset$c2*contact2[t]
+      lambda1[t,] <- as.vector((1+b1*cos(2*pi*(t-phi*52.1775)/52.1775))*((I1[t,]+parmset$rho1*I2[t,]+parmset$rho2*I3[t,]+parmset$rho2*I4[t,]+intro2[t])%*%beta)/sum(St[t,]))}
+    
+    
+    hosp = c(rep(parmset$h1,3),rep(parmset$h2,3),rep(parmset$h3,2),rep(parmset$h4,4),parmset$h5)
+    
+    H1=matrix(0,nrow=t0,ncol=al)#Number of hospitalizations by age
+    
+    for (i in 1:al){
+      H1[,i]=
+        hosp[i]*parmset$RRHm*parmset$sigma3*M[,i]*lambda1[,i]+
+        hosp[i]*parmset$RRHn*parmset$RRIn*parmset$sigma3*Mn[,i]*lambda1[,i]+
+        hosp[i]*parmset$RRHv*parmset$RRIv*parmset$sigma3*Mv[,i]*lambda1[,i]+
+        hosp[i]*parmset$RRHn*parmset$RRIn*parmset$RRHm*N[,i]*lambda1[,i]+
+        hosp[i]*S0[,i]*lambda1[,i]+
+        hosp[i]*Si[,i]*lambda1[,i]+
+        hosp[i]*parmset$sigma1*S1[,i]*lambda1[,i]+
+        hosp[i]*parmset$sigma2*S2[,i]*lambda1[,i]+
+        hosp[i]*parmset$sigma3*S3[,i]*lambda1[,i]+
+        hosp[i]*parmset$RRIs*parmset$RRHs*parmset$sigma3*Vs1[,i]*lambda1[,i]+
+        hosp[i]*parmset$RRIs*parmset$RRHs*parmset$sigma3*Vs2[,i]*lambda1[,i]}
+    
+    
+    H2= cbind(rowSums(H1[,1:3]),rowSums(H1[,4:6]),rowSums(H1[,7:8]),rowSums(H1[,9:12]),H1[,13]) 
+    new_results = data.frame(cbind(H2[(t0-(tmax3-tmax2)):t0,],rowSums(H2[(t0-(tmax3-tmax2)):t0,])))
+    names(new_results)=c("<6m",">6m","1-4yr","5-59yrs","60+yrs","All ages")
+    new_results$date = dates$date[tmax2:tmax3]
+    new_results$sample = l
+    
+    results = rbind(results,new_results)
+    
+  }
+  return(results)
+}
